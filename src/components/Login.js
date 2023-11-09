338db56ed48e1d36db8fb721b9166709e413f6b2
Michael Muchiri <muchirimichael2222@gmail.com>
2023-11-09 15:49:34 +0300

    Completed

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ function Login({ setIsLoggedIn }) {
   const [formData, setFormData] = useState({
     username: "",
     password: "",
-  });
+  })
 
   function handleChange(e) {
     setFormData({
